abeb05d5619103b1d88265ceb7cd901b02bf0b75
ronlemire2 <ronlemire2@gmail.com>
2024-10-22 15:39:33 -0700

    Initial Commit

---
+++ TestGithub1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestGithub1
+//
+//  Created by Ron Lemire on 10/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestGithub1/TestGithub1App.swift
@@ -0,0 +1,17 @@
+//
+//  TestGithub1App.swift
+//  TestGithub1
+//
+//  Created by Ron Lemire on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct TestGithub1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
